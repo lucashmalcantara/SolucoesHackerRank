42407da01f6ff535a5ffbfd5be754a081c8a799f
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-07-06 17:16:30 -0300

    Create Draw The Triangle 1.sql

---
+++ SQL/Alternative Queries/Draw The Triangle 1.sql
@@ -0,0 +1,20 @@
+DECLARE @quantidade INT = 20;
+DECLARE @i INT = 0;
+DECLARE @j INT = 0;
+DECLARE @triangulo NVARCHAR(MAX) = '';
+
+WHILE @i < @quantidade
+BEGIN
+    SET @j = @i;
+    
+    WHILE @j < @quantidade
+    BEGIN
+        SET @triangulo = @triangulo + '* ';
+        SET @j += 1;
+    END
+    
+    SET @triangulo = @triangulo + CHAR(13) + CHAR(10);
+    SET @i += 1;
+END
+
+PRINT @triangulo;
